1630e0e95773cb909742d82ff44b90603264d32a
mlentzler <michel.lentzler@gmail.com>
2025-12-01 16:08:04 +0100

    adds src

---
+++ colors.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Catppuccin Frappe
+export BLACK=0xff303446
+export WHITE=0xffc6d0f5
+export RED=0xffe78284
+export GREEN=0xffa6d189
+export BLUE=0xff8aadf4
+export YELLOW=0xffe5c890
+export ORANGE=0xffef9f76
+export MAGENTA=0xffca9ee6
+export GREY=0xff51576d
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xD0303446
+export ACCENT_COLOR=$BLUE
+export ITEM_BG_COLOR=0xff414559
+export ACCENT=$BLUE
+export GREY_TRANSP=0x3351576d
+export LIGHT_WHITE=0xfff2d5cf
